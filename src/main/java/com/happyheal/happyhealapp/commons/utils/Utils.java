77f6f46d593cd67ee46a194bebcf4e0f8918e16d
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-02-05 04:16:08 +0530

    to gray scale function added

--- src/main/java/com/happyheal/happyhealapp/commons/utils/Utils.java
+++ src/main/java/com/happyheal/happyhealapp/commons/utils/Utils.java
@@ -2,6 +2,7 @@ package com.happyheal.happyhealapp.commons.utils;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.*;
 import android.net.Uri;
 import android.provider.MediaStore;
 
@@ -9,6 +10,14 @@ import android.provider.MediaStore;
  * Created by pnagarjuna on 02/02/16.
  */
 public class Utils {
+
+    /**
+     * Get file path from URI
+     *
+     * @param mContext
+     * @param mContentUri
+     * @return
+     */
     public static String getRealPathFromURI(final Context mContext, final Uri mContentUri) {
         String res = null;
         String[] proj = {MediaStore.Images.Media.DATA};
@@ -20,4 +29,27 @@ public class Utils {
         cursor.close();
         return res;
     }
+
+    /**
+     * Converts the Colored Image into Gray Scale
+     *
+     * @param bmpOriginal
+     * @return
+     */
+    public Bitmap toGrayScale(Bitmap bmpOriginal) {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayScale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayScale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayScale;
+    }
+
 }
